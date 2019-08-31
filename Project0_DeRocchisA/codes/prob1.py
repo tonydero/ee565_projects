a6a663a36f1dc77d47c738d192477e78dde0ce85
tonydero <tonyderocchis@gmail.com>
2019-08-31 15:13:36 -0600

    project 0 work

--- Project0_DeRocchisA/codes/prob1.py
+++ Project0_DeRocchisA/codes/prob1.py
@@ -2,10 +2,17 @@
 # on 2019.08.24 at 23:33 UTC
 import numpy as np
 from bokeh.plotting import figure, output_file, show
+from requiredFunctions.circGauss import circGauss
+import matplotlib.pyplot as plt
 
 
-def main():
-    """
-    Main script for the solution to Project 0 for EE 565 Fall 2019
-    """
+"""
+Main script for the solution to Project 0 for EE 565 Fall 2019
+"""
+samples_0 = circGauss(434, (0, 0), 1)
+samples_5 = circGauss(434, (5, 2), 1)
+
+plt.scatter(samples_0[0], samples_0[1])
+plt.scatter(samples_5[0], samples_5[1])
+plt.show()
 
--- Project0_DeRocchisA/codes/requiredFunctions/circGauss.py
+++ Project0_DeRocchisA/codes/requiredFunctions/circGauss.py
@@ -1,17 +1,26 @@
 import numpy as np
 
 
-def circGauss(N):
+def circGauss(N, mean=(0,0), variance=1):
     """
-    a) Write a function names "circGauss" which returns N samples from a
+    a) Write a function named "circGauss" which returns N samples from a
     circular symmetric multi-variate Gaussian distribution with a specified
     mean and variance.
     """
-    samples = {}
-    for i in range(mean, variance, N):
-        new_sample = np.random.normal(mean, variance)
+    samples_x1 = []
+    samples_x2 = []
+    np.random.seed(83704)
 
-        samples += new_sample
+    for i in range(N):
+        new_sample_x1 = np.random.normal(mean[0], variance)
+        new_sample_x2 = np.random.normal(mean[1], variance)
 
-        return samples
+        samples_x1.append(new_sample_x1)
+        samples_x2.append(new_sample_x2)
+
+    samples_x1 = np.array(samples_x1)
+    samples_x2 = np.array(samples_x2)
+    samples = np.array((samples_x1,samples_x2))
+
+    return samples
 
